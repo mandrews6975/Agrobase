7a7ec7ef16ab25a4ed10a5256ae7aff56bcc5372
lewissheaffer <lewiss@iastate.edu>
2019-11-02 17:49:47 -0500

    Added renderer functions for displaying currents days schedule.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const fs = require('fs');
-const ipc = require('electron').ipcMain;
+const ipcMain = require('electron').ipcMain;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -44,7 +44,7 @@ const createWindow = () => {
 };
 
 const fileWriter = (event, arg) => {
-    fs.appendFile("data/" + arg[0], arg[1] + " " + arg[2] , function(err) {
+    fs.appendFile("data/" + arg[0], arg[1] + " " + arg[2] + '\n', function(err) {
     if(err) {
       return console.log(err);
     }
