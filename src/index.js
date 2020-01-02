2c9ab75180c5e50285ec0c4a5b78a1c7d9a2adc3
Michael Andrews <43254509+mandrews6975@users.noreply.github.com>
2020-01-02 13:05:43 -0600

    Made several UI changes, added base modal dialog for editing groups, changed button visibility based on current view in contacts card.

--- src/index.js
+++ src/index.js
@@ -101,15 +101,52 @@ function switchAllContacts(){
 }
 
 function switchContactGroups(){
+  document.getElementById('delete_button').style = 'display: ; float: right';
+  document.getElementById('edit_group_button').style = 'display: none';
   document.getElementById('current_contacts_tab').innerHTML = 'Contact Groups';
-  getGroupData("Drivers");
-  console.log(getGroupNames());
+  let numElements = document.getElementById('contacts_table').children.length;
+  for(let i = 0; i < numElements; i++){
+    document.getElementById('contacts_table').children[0].remove();
+  }
+  let groups = getGroupNames();
+  for(const group of groups){
+    let row = document.createElement('tr');
+    let groupListing = document.createElement('td');
+    groupListing.appendChild(document.createTextNode(group));
+    groupListing.addEventListener('click', () => {
+      getGroupData(groupListing.innerHTML.substring(0, groupListing.innerHTML.indexOf('<')));
+    });
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.style = 'float: right';
+    groupListing.appendChild(checkbox);
+    row.appendChild(groupListing);
+    document.getElementById('contacts_table').appendChild(row);
+  }
 }
 
 function getAllContacts(){
   ipcRenderer.send('get_all_contacts');
   ipcRenderer.on('all_contacts', (event, args) => {
+    document.getElementById('delete_button').style = 'display: ; float: right';
+    document.getElementById('edit_group_button').style = 'display: none';
+    let numElements = document.getElementById('contacts_table').children.length;
+    for(let i = 0; i < numElements; i++){
+      document.getElementById('contacts_table').children[0].remove();
+    }
     //args contains a list of all contacts
+    for(const contact of args.contacts){
+      let row = document.createElement('tr');
+      let contactListing = document.createElement('td');
+      contactListing.appendChild(document.createTextNode(contact.fname + ' ' + contact.lname + '\n' + contact.phone + '   ' + contact.email));
+      contactListing.style = 'white-space: pre';
+      let checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.style = 'float: right';
+      contactListing.appendChild(checkbox);
+      row.appendChild(contactListing);
+      document.getElementById('contacts_table').appendChild(row);
+    }
   });
 }
 
@@ -137,8 +174,35 @@ function addNewContact(){
 function getGroupData(groupName){
   ipcRenderer.send('get_group_data', groupName);
   ipcRenderer.on('group_data', (event, args) => {
+    document.getElementById('delete_button').style = 'display: none';
+    document.getElementById('current_contacts_tab').innerHTML = groupName;
+    let numElements = document.getElementById('contacts_table').children.length;
+    for(let i = 0; i < numElements; i++){
+      document.getElementById('contacts_table').children[0].remove();
+    }
     //args contains parsed group json object
-    console.log(args);
+    document.getElementById('edit_group_button').style = 'display: ';
+    for(const message of args.messages){
+      let row = document.createElement('tr');
+      let messageListing = document.createElement('td');
+      let card = document.createElement('div');
+      card.classList.add('card');
+      card.classList.add('bg-info');
+      card.classList.add('mb-2');
+      let cardBody = document.createElement('div');
+      cardBody.classList.add('px-1');
+      cardBody.classList.add('py-1');
+      cardBody.classList.add('card-body');
+      let cardTitle = document.createElement('h5');
+      cardTitle.classList.add('card-title');
+      cardTitle.appendChild(document.createTextNode(message.message));
+      cardBody.appendChild(cardTitle);
+      cardBody.appendChild(document.createTextNode('Sent: ' + message.time));
+      card.appendChild(cardBody);
+      messageListing.appendChild(card);
+      row.appendChild(messageListing);
+      document.getElementById('contacts_table').appendChild(row);
+    }
   });
 }
 
@@ -152,12 +216,12 @@ function getGroupNames() {
 }
 
 function addNewGroup() {
-  arg = document.getElementById('input_groupName').value;
+  arg = document.getElementById('input_groupName_newGroup').value;
   if(fs.existsSync("data/contacts/groups/" + arg + ".json")){
-    document.getElementById("input_groupName").placeholder = "Group Name Already Exists";
+    document.getElementById("input_groupName_newGroup").placeholder = "Group Name Already Exists";
+    document.getElementById("input_groupName_newGroup").value = '';
   }
   else {
-    document.getElementById("input_groupName").placeholder = "Group Name";
     jQuery('#modal_new_group').modal('hide');
     ipcRenderer.send('addNewGroup', arg);
     ipcRenderer.on('groupWritingDone', () => {
@@ -165,4 +229,42 @@ function addNewGroup() {
     });
   }
   document.getElementById("input_groupName").value = "";
+}
+
+function editGroup(){
+  document.getElementById('input_groupName_editGroup').value = document.getElementById('current_contacts_tab').innerHTML;
+  ipcRenderer.send('get_all_contacts');
+  ipcRenderer.on('all_contacts', (event, allContacts) => {
+    let numElements = document.getElementById('edit_group_contacts_table').children.length;
+    for(let i = 0; i < numElements; i++){
+      document.getElementById('edit_group_contacts_table').children[0].remove();
+    }
+    ipcRenderer.send('get_group_data', document.getElementById('current_contacts_tab').innerHTML);
+    ipcRenderer.on('group_data', (event, groupData) => {
+      for(const contact of allContacts.contacts){
+        let row = document.createElement('tr');
+        let contactListing = document.createElement('td');
+        contactListing.appendChild(document.createTextNode(contact.fname + ' ' + contact.lname + '\n' + contact.phone + '   ' + contact.email));
+        contactListing.style = 'white-space: pre';
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.style = 'float: right';
+        let j = 0;
+        for(groupContact of groupData.contacts){
+          if(groupData.contacts[j].fname == contact.fname && groupData.contacts[j].lname == contact.lname){
+            checkbox.checked = true;
+            break;
+          }
+          j++;
+        }
+        contactListing.appendChild(checkbox);
+        row.appendChild(contactListing);
+        document.getElementById('edit_group_contacts_table').appendChild(row);
+      }
+    });
+  });
+}
+
+function saveGroupChanges(){
+
 }
